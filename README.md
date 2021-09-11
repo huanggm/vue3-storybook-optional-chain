27dc18cea4e3ac3f04545cc384d04076913c3b4c
kaokei <kaokei.rst@gmail.com>
2021-09-12 02:45:16 +0800

    init project

--- README.md
+++ README.md
@@ -1,24 +1,41 @@
 # vue3-storybook-optional-chain
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## This repo is for reproducing bug
+
+we can not use optional chain inside vue storybook.
+
+step1: use vuecli to create project. `vue create project`
+
+step2: use npx to init storybook. `npx sb init`
+
+step3: use optional chain inside vue component
+
+step4: `npm run storybook`
---
+++ src/stories/Button.vue
@@ -0,0 +1,62 @@
+<template>
+  <button type="button" :class="classes" @click="onClick" :style="style">{{ label }} - {{test?.name}}</button>
+</template>
+
+<script lang="ts">
+import './button.css';
+import { reactive, computed } from 'vue';
+
+interface Test {
+  name?: string;
+}
+
+export default {
+  name: 'my-button',
+
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    primary: {
+      type: Boolean,
+      default: false,
+    },
+    size: {
+      type: String,
+      validator: function (value) {
+        return ['small', 'medium', 'large'].indexOf(value) !== -1;
+      },
+    },
+    backgroundColor: {
+      type: String,
+    },
+  },
+
+  emits: ['click'],
+
+  setup(props, { emit }) {
+    props = reactive(props);
+
+    const test: Test = {
+        name: 'hello'
+    }
+
+    return {
+      test,
+      classes: computed(() => ({
+        'storybook-button': true,
+        'storybook-button--primary': props.primary,
+        'storybook-button--secondary': !props.primary,
+        [`storybook-button--${props.size || 'medium'}`]: true,
+      })),
+      style: computed(() => ({
+        backgroundColor: props.backgroundColor,
+      })),
+      onClick() {
+        emit('click');
+      }
+    }
+  },
+};
+</script>
